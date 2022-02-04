c8b0732b51fd4957071e6f038ef5afef28ad8322
Ethan Wolfe <ethan.wolfe212@gmail.com>
2022-02-04 00:23:26 -0800

    Add logging and calculation times

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 import os
 import platform
 import logging
+from math import floor
 from pmaw import PushshiftAPI
 from datetime import datetime
 
@@ -19,19 +20,33 @@ date_end = int(datetime(2022, 2, 1, 5, 0).timestamp())
 
 post_limit = 100
 comment_limit = 500
+max_log_files = 25
 
 # Set up the logger
 if not os.path.exists('logs'):
     os.mkdir('logs')
-log_name = datetime.now().strftime('logs/reddit-scraper-%Y-%m-%d-%H-%M-%S.log')
-logging.basicConfig(
-    format='%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s',
-    filename=log_name, filemode='w'
-)
+else:
+    # Ensure we don't have too many log files
+    log_files = os.listdir('logs')
+    if len(log_files) > max_log_files:
+        os.remove(f'logs/{log_files[0]}')
+
+logger = None
+while True:
+    log_name = datetime.now().strftime('logs/reddit-scraper-%Y-%m-%d-%H-%M-%S.log')
+    format_string = '[%(levelname)-10s] [%(threadName)s] [%(name)-14s] - %(message)s'
+    formatter = logging.Formatter(format_string)
+    handler = logging.FileHandler(log_name)
+    handler.setFormatter(formatter)
+
+    logging.basicConfig(format=format_string, force=True)
+    logger = logging.getLogger("Reddit Scraper")
+    logger.addHandler(handler)
+    break
 
 
 def log(message, level=logging.INFO):
-    logging.log(level, message)
+    logger.log(level, f'{message}')
 
 
 # Access the PushShift Reddit API
@@ -39,16 +54,40 @@ api = PushshiftAPI(
     num_workers=thread_count
 )
 
+log(f'Initialized the PMAW scraper with {thread_count} threads')
+log(f'Post limit per fetch is {post_limit}')
+log(f'Comment limit per fetch is {comment_limit}')
+
+# TODO: Initialize a SQLite database
+# TODO: Get the list of subreddits from the database
 # later list ['Flash', 'FlashArchival', 'FlashPlayer', 'FlashPlayerForever']
 subreddits = ['AdobeFlash']  # make a list of subreddits you want to scrape the data from
 
+
+def end_time(start, message):
+    end = datetime.now()
+    difference = end - start
+    log(f'It took {floor(difference.seconds / 60)} m {difference.seconds % 60} s to {message}')
+
+
 for subreddit in subreddits:
-    posts = api.search_submissions(
-        subreddit=subreddit,
-        limit=post_limit
-    )
+    start = datetime.now()
+    log(f'Gathering posts for r/{subreddit} subreddit')
+    posts = api.search_submissions(subreddit=subreddit, limit=post_limit)
+    post_count = len(posts)
+    log(f'Gathered {post_count} posts for r/{subreddit} subreddit')
+    end_time(start, 'get posts')
 
+    start = datetime.now()
+    log(f'Gathering comment ids for {post_count} posts for r/{subreddit} subreddit')
     post_ids = [post['id'] for post in posts]
-    comment_ids = api.search_submission_comment_ids(post_ids)
+    comment_ids = api.search_submission_comment_ids(post_ids, limit=comment_limit)
+    comment_count = len(comment_ids)
+    log(f'Gathered {comment_count} comment ids for {post_count} posts for r/{subreddit} subreddit')
+    end_time(start, 'get comment ids')
 
-    comments = api.search_comments(ids=comment_ids)
+    start = datetime.now()
+    log(f'Gathering {comment_count} comments for {post_count} posts for r/{subreddit} subreddit')
+    comments = api.search_comments(ids=comment_ids, limit=comment_limit)
+    log(f'Gathered {comment_count} comments for {post_count} posts for r/{subreddit} subreddit')
+    end_time(start, 'get comments')
